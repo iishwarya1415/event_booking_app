5bca03cb76a1fe12b906c36f6ee572a86d60e28c
Riddhi <riddhi400@gmail.com>
2025-03-25 17:12:06 -0400

    index start

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { register, login, logout, getMe } from "../api/routes/auth.js";
+import { requireAuth } from "../api/middleware/requireAuth.js";
+import {ping} from "../api/routes/ping.js";
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+
+app.post("/register", register);
+app.post("/login", login);
+app.post("/logout", logout);
+app.get("/me", requireAuth, getMe);
+app.get("/ping",ping);
+
+export default app;
+app.listen(8000, () => {
+  console.log("Server running on http://localhost:8000 🎉 🚀");
+});
