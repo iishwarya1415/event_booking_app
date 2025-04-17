ce8cc6d97b7f74aa6dc283cc07aa96013acd1d84
Riddhi <riddhi400@gmail.com>
2025-04-16 20:18:29 -0400

    add env variables to client

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set base URL for all axios requests
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL|| 'http://localhost:8080';
 
 // Example: Fetch all events
 const fetchEvents = async () => {
